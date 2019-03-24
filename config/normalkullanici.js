88f657d380e905dbc64047827bf472ef20d0c79f
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-03-24 17:22:43 +0300

    feat: Kullanıcı şifresi hashlenme işlemi yapıldı.

--- config/normalkullanici.js
+++ config/normalkullanici.js
@@ -1,5 +1,6 @@
 const mongoose  = require('mongoose');
 const Schema    = mongoose.Schema;
+const brcypt    = require('bcrypt');
 
 //Normal Kullnıcı için MongoDB Şeması
 const normalKullanici = new Schema({
@@ -28,6 +29,24 @@ const normalKullanici = new Schema({
         type: String,
         required: true
     }                  
+});
+
+normalKullanici.pre('save',function(next){
+    var kullanici =this;
+    brcypt.genSalt(10,function(err,salt){
+        if(err) return next(err);   
+        brcypt.hash(kullanici.k_password,salt,function(err,hash){
+            if(err){
+                return next(err);
+                console.log("Hash Hatası");
+            }
+
+            kullanici.k_password=hash;
+                console.log(kullanici.k_email+"E-Mailine Sahip Kullanıcının Şifresi Hashlendi.");
+                next();
+            
+        });
+    });
 });
 
 
