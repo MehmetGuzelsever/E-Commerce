52b56eb349e4ca625bb1698934b29851873f54bd
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-03-24 17:23:01 +0300

    feat: Admin şifresi hashlenme işlemi yapıldı.

--- config/admin.js
+++ config/admin.js
@@ -22,6 +22,24 @@ const admin = new Schema({
     }             
 });
 
+admin.pre('save',function(next){
+    var admins =this;
+    brcypt.genSalt(10,function(err,salt){
+        if(err) return next(err);   
+        brcypt.hash(admins.a_password,salt,function(err,hash){
+            if(err){
+                return next(err);
+                console.log("Hash Hatası");
+            }
+
+            admins.a_password=hash;
+                console.log(admins.a_email+"E-Mailine Sahip Adminin Şifresi Hashlendi.");
+                next();
+            
+        });
+    });
+});
+
 
 //Admin için MongoDB Modeli
 module.exports = mongoose.model('ADMIN', admin);
