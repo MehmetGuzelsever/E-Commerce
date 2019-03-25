ccb3d74525f704eb06f653833b8127629febc265
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-03-25 20:13:20 +0300

    feat: Added Authentication Requests.

--- routes.js
+++ routes.js
@@ -10,7 +10,79 @@ const orders    = require("./orders/index");
 
 //Initial Request
 router.get('/', function(req, res) {
-    res.json({"msg":"Welcome to E-Commerce api"});
+    res.json({msg:"Welcome to E-Commerce api"});
+})
+
+//Normal Kullanıcı Register
+router.post('/user/register', function(req, res) {
+    auth.user(req.query, function (error, response) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(response);
+        }
+    })
+})
+
+//Ev Hanımı Register
+router.post('/housewife/register', function(req, res) {
+    auth.housewife(req.query, function (error, response) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(response);
+        }
+    })
+})
+
+//Admin Register
+router.post('/admin/register', function(req, res) {
+    auth.admin(req.query, function (error, response) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(response);
+        }
+    })
+})
+
+//Kullanıcı Login
+router.get('/user/login', function(req, res) {
+    auth.userLogin(req.query, function (error, response) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(response);
+        }
+    })
+})
+
+//Ev Hanımı Login
+router.get('/housewife/login', function(req, res) {
+    auth.evlogin(req.query, function (error, response) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(response);
+        }
+    })
+})
+
+//Admin Login
+router.get('/admin/login', function(req, res) {
+    auth.adminLogin(req.query, function (error, response) {
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send(response);
+        }
+    })
 })
 
 
