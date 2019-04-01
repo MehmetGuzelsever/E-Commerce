e5e544506058d6600e5180053576b477470d13b8
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-04-01 22:06:35 +0300

    feat: Angular Housewife Login Controller.

--- public/controllers/housewifeController.js
+++ public/controllers/housewifeController.js
@@ -22,7 +22,7 @@ angular.module('housewifeController', [])
         app.loading = true;
         console.log(app.regData)
         if (app.regData.email == null || app.regData.adi == null || app.regData.soyadi == null || app.regData.password == null
-            || app.regData.il == null || app.regData.ilce == null app.regData.adres == null ) {
+            || app.regData.il == null || app.regData.ilce == null || app.regData.adres == null ) {
                 app.infoMessage = true;
                 app.loading = false;
             }
@@ -43,4 +43,34 @@ angular.module('housewifeController', [])
                 })
             }
     }
+})
+
+//Login Controller
+.controller('logHouseController', function($location, AuthHouse) {
+    var app = this;
+    app.infoMessage = false;
+    app.errMsg = false;
+    app.sucMsg = false;
+    app.loginUser = function() {
+        if (app.loginData.email == null || app.loginData.password == null) {
+                app.infoMessage = true;
+                app.loading = false;
+            }
+            else {
+                AuthHouse.getLoginReq('/api/housewife/login', app.loginData)
+                .then(function(info) {
+                    if (info.data.success == false) {
+                        app.errMsg = true;
+                        app.error = info.data.msg;
+                        app.loading = false;
+                    }
+                    else {
+                        app.loading = false;
+                        app.sucMsg = true;
+                        app.success = info.data.msg;
+                        $location.path('/');  
+                    }
+                })
+            }        
+    }    
 })
