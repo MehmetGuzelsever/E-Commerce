f0746015f0c453d94949e30d788a307eb9baba51
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-04-01 21:53:21 +0300

    feat: Angular Housewife Register Controller.

---
+++ public/controllers/housewifeController.js
@@ -0,0 +1,46 @@
+angular.module('housewifeController', [])
+
+//Register Controller
+.controller('regHouseController', function($location, House) {
+    var app = this;
+    app.il = ["İstanbul", "Ankara", "İzmir"];
+    app.infoMessage = false;
+    app.errMsg = false;
+    app.sucMsg = false;
+    app.ilce = function(il) {
+        if (il == "İzmir") {
+            app.ilceler = ["Bornova", "Buca", "Konak", "Tire"];
+        }
+        else if (il == "İstanbul") {
+            app.ilceler = ["Kadıköy", "Pendik", "Üsküdar", "Kartal"];
+        }
+        else if (il == "Ankara") {
+            app.ilceler = ["Çankaya", "Keçiören", "Yenimahalle", "Mamak"];
+        }
+    }
+    app.regUser = function() {
+        app.loading = true;
+        console.log(app.regData)
+        if (app.regData.email == null || app.regData.adi == null || app.regData.soyadi == null || app.regData.password == null
+            || app.regData.il == null || app.regData.ilce == null app.regData.adres == null ) {
+                app.infoMessage = true;
+                app.loading = false;
+            }
+            else {
+                House.getRegisterReq('/api/housewife/register', app.regData)
+                .then(function(info) {
+                    if (info.data.success == false) {
+                        app.errMsg = true;
+                        app.error = info.data.msg;
+                        app.loading = false;
+                    }
+                    else {
+                        app.sucMsg = true;
+                        app.success = info.data.msg;
+                        app.loading = false;
+                        $location.path('/') 
+                    }
+                })
+            }
+    }
+})
