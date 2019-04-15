1430cde241177ce0b30cff27da73e01bd7aabb0d
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-04-16 00:42:26 +0300

    feat: Web App Header User Token.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,5 @@
-angular.module('app', ['appRoutes', 'mainController', 'userController', 'housewifeController', 'userService', 'housewifeService']);
+angular.module('app', ['appRoutes', 'mainController', 'userController', 'housewifeController', 'userService', 'housewifeService'])
+
+.config(function($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptors');
+})
