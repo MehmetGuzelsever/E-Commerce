e3ec4e8670b1124ca61f3a1a308b23e0451948b7
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-04-16 23:38:46 +0300

    feat: Web App User Update Controller.

--- public/controllers/userController.js
+++ public/controllers/userController.js
@@ -74,3 +74,61 @@ angular.module('userController', [])
             }        
     }
 })
+
+//Update Controller
+.controller('updateController', function($location,  Auth, Update) {
+    var app = this;
+    app.err = false;
+    app.suc = false;
+    app.ils = ["İstanbul", "Ankara", "İzmir"];
+    app.ilcen = function(il) {
+        if (il == "İzmir") {
+            app.ilces = ["Bornova", "Buca", "Konak", "Tire"];
+        }
+        else if (il == "İstanbul") {
+            app.ilces = ["Kadıköy", "Pendik", "Üsküdar", "Kartal"];
+        }
+        else if (il == "Ankara") {
+            app.ilces = ["Çankaya", "Keçiören", "Yenimahalle", "Mamak"];
+        }
+    }
+    if(Auth.isLoggedIn()) {
+        Auth.getUser()
+        .then(function(data) {
+            app.showregpage = false;
+            app.email = data.data.email;
+            app.name = data.data.name;
+            app.surname = data.data.surname;
+            app.il = data.data.il;
+            app.ilce = data.data.ilce;            
+        })
+    }
+
+    app.update = function() {
+        if (app.updateData.name == null) {
+            app.updateData.name = app.name;
+        }
+        if (app.updateData.surname  == null) {
+            app.updateData.surname = app.surname;
+        }
+        if (app.updateData.il == null) {
+            app.updateData.il = app.il;
+        }
+        if (app.updateData.ilce == null) {
+            app.updateData.ilce = app.ilce;
+        }
+        app.updateData.email = app.email;
+        Update.update('/api/user/update', app.updateData)
+        .then(function(info) {
+            if(info.data.success == false) {
+                app.err = true;
+                app.errorMsg = info.data.msg;
+            }
+            else {
+                app.suc = true;
+                app.SuccessMsg = info.data.msg;
+            }
+        })                 
+    }
+
+})
