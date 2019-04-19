c3a9d4ddea4badfef381e463c74c8be3237fd45c
Mehmet Güzelsever <34800843+MehmetGuzelsever@users.noreply.github.com>
2019-04-20 00:21:34 +0300

    feat: Web App Housewife Account update in angular controller.

--- public/controllers/housewifeController.js
+++ public/controllers/housewifeController.js
@@ -46,7 +46,7 @@ angular.module('housewifeController', [])
 })
 
 //Login Controller
-.controller('logHouseController', function($location, AuthHouse) {
+.controller('logHouseController', function($location, Auth) {
     var app = this;
     app.infoMessage = false;
     app.errMsg = false;
@@ -57,7 +57,7 @@ angular.module('housewifeController', [])
                 app.loading = false;
             }
             else {
-                AuthHouse.getLoginReq('/api/housewife/login', app.loginData)
+                Auth.getLoginReq('/api/housewife/login', app.loginData)
                 .then(function(info) {
                     if (info.data.success == false) {
                         app.errMsg = true;
@@ -73,4 +73,66 @@ angular.module('housewifeController', [])
                 })
             }        
     }    
+})
+
+//Update Controller
+.controller('updateHousewifeController', function($location,  Auth, UpdateHousewife) {
+    var app = this;
+    app.err = false;
+    app.suc = false;
+    app.ils = ["İstanbul", "Ankara", "İzmir"];
+    app.ilcen = function(il) {
+        if (il == "İzmir") {
+            app.ilces = ["Bornova", "Buca", "Konak", "Tire"];
+        }
+        else if (il == "İstanbul") {
+            app.ilces = ["Kadıköy", "Pendik", "Üsküdar", "Kartal"];
+        }
+        else if (il == "Ankara") {
+            app.ilces = ["Çankaya", "Keçiören", "Yenimahalle", "Mamak"];
+        }
+    }
+    if(Auth.isLoggedIn()) {
+        Auth.getUser()
+        .then(function(data) {
+            app.showregpage = false;
+            app.email = data.data.email;
+            app.name = data.data.name;
+            app.surname = data.data.surname;
+            app.il = data.data.il;
+            app.ilce = data.data.ilce;   
+            app.adres = data.data.adres;         
+        })
+    }
+
+    app.update = function() {
+        if (app.updateData.name == null) {
+            app.updateData.name = app.name;
+        }
+        if (app.updateData.surname  == null) {
+            app.updateData.surname = app.surname;
+        }
+        if (app.updateData.il == null) {
+            app.updateData.il = app.il;
+        }
+        if (app.updateData.ilce == null) {
+            app.updateData.ilce = app.ilce;
+        }
+        if (app.updateData.adres == null) {
+            app.updateData.adres = app.adres;
+        }        
+        app.updateData.email = app.email;
+        UpdateHousewife.update('/api/housewife/update', app.updateData)
+        .then(function(info) {
+            if(info.data.success == false) {
+                app.err = true;
+                app.errorMsg = info.data.msg;
+            }
+            else {
+                app.suc = true;
+                app.SuccessMsg = info.data.msg;
+            }
+        })                 
+    }
+
 })
