40b7fbdb03376994cf20db91e579fba1041e24f1
MehmetGuzelsever <mguzelsever35@gmail.com>
2019-04-28 22:54:21 +0300

    refactor: Web App added angular admin controller in angular app module.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['appRoutes', 'mainController', 'userController', 'housewifeController', 'mainService', 'userService', 'housewifeService'])
+angular.module('app', ['appRoutes', 'mainController', 'userController', 'housewifeController', 'mainService', 'userService', 'housewifeService', 'adminController'])
 
 .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptors');
